4070d2f33183d866190c5fc49915f10343eff82b
Matthias Bräuer <matthias.braeuer@arcor.de>
2015-06-15 17:16:45 +0200

    added webapicontroller

---
+++ UR_Talking/UR_Talking/Scripts/questionHandler.js
@@ -0,0 +1,37 @@
+﻿var onButtonClick = function (ev) {
+    var input = $('input[type=search]').val();
+
+    ev.preventDefault();
+
+    renderQuestion(input);
+
+    sendQuestion(input);
+},
+    sendQuestion = function (question) {
+        $.ajax({
+            url: "api/values",
+            type: "POST",
+            data: { '': question },
+            success: onAnswerSuccess,
+            error: onAnswerError
+        });
+    },
+    onAnswerSuccess = function (response) {
+        renderAnswer(response);
+    },
+    onAnswerError = function (err) {
+        console.log(err);
+    },
+    renderAnswer = function (answer) {
+        var answerLi = $('<li><strong>' + "Elise: " + '</strong>' + answer + '</li>');
+        $('#chatHistory').append(answerLi);
+    },
+    renderQuestion = function (question) {
+        var user = $('<strong>Du: </strong>'),
+            questionLi = $('<li><strong>' + "Du: " + '</strong>' + question + '</li>');
+        $('#chatHistory').append(questionLi);
+    };
+
+(function () {
+    $('button[type=submit]').click(onButtonClick);
+}());
