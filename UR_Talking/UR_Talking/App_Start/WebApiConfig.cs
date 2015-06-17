c9268d640fd3545aec41814e4c4cdfc35c2710d9
Matthias Bräuer <matthias.braeuer@arcor.de>
2015-06-17 15:20:46 +0200

    added Dependency Injection for values Controller

--- UR_Talking/UR_Talking/App_Start/WebApiConfig.cs
+++ UR_Talking/UR_Talking/App_Start/WebApiConfig.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Net.Http;
+using Microsoft.Practices.Unity;
+using UR_Talking.Resolver;
+using UR_Talking.DAO;
+using UR_Talking.DAO_Impl;
+using Iveonik.Stemmers;
 
 namespace UR_Talking.App_Start
 {
@@ -11,6 +16,13 @@ namespace UR_Talking.App_Start
     {
         public static void Register(HttpConfiguration config)
         {
+            UnityContainer container = new UnityContainer();
+            
+            container.RegisterType<AnswerDAO, AnswerDAOImpl>(new HierarchicalLifetimeManager());
+            container.RegisterType<IStemmer, GermanStemmer>(new HierarchicalLifetimeManager());
+
+            config.DependencyResolver = new UnityResolver(container);
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ UR_Talking/UR_Talking/DAO_Impl/AnswerDAOImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UR_Talking.DAO;
+
+namespace UR_Talking.DAO_Impl
+{
+    public class AnswerDAOImpl : AnswerDAO
+    {
+        public string GetAnswer(string[] question)
+        {
+            return this.ToString();
+        }
+    }
+}
